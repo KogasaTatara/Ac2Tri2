bcdbe2493878235966c6d0776cd8581139bbf54a
KogasaTatara <tanuke.p2@gmail.com>
2025-06-09 11:02:28 -0300

    FumoFumo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { Animation, createAnimation } from '@ionic/core';
 
 @Component({
   selector: 'app-tab1',
@@ -8,24 +9,34 @@ import { AlertController } from '@ionic/angular';
   standalone: false,
 })
 export class Tab1Page {
+  @ViewChild('palhacada', { static: false }) palhacada!: ElementRef<HTMLImageElement>;
 
-  constructor(private alertController: AlertController) {}
-emprestimo = 0;
-juros = 0;
-messes = 0;
-ValorParcela = 0;
+  constructor(private alertController: AlertController) { }
+  emprestimo = 0;
+  juros = 0;
+  messes = 0;
+  ValorParcela = 0;
 
-async Calcular() {
-  if ((this.juros/100) < 0.20) {
-    this.ValorParcela = (this.emprestimo * (this.juros/100)) / (1 - (1 + (this.juros/100)) ** (-this.messes));
-  } else {
-    this.ValorParcela = (this.emprestimo * (this.juros/100)) / (1 - (1 + (this.juros/100)) ** (-this.messes));
-    const alert = await this.alertController.create({
-      header: 'Cuidado',
-      message: 'Esse empréstimo pode ser arriscado',
-      buttons: ['OK']
-    });
-    await alert.present();
-  };
-};
-};
+
+  async Calcular() {
+    if (this.palhacada) {
+      const animation: Animation = createAnimation()
+        .addElement(this.palhacada.nativeElement)
+        .duration(1000)
+        .fromTo('opacity', '0', '1');
+      if ((this.juros / 100) < 0.20) {
+        this.ValorParcela = (this.emprestimo * (this.juros / 100)) / (1 - (1 + (this.juros / 100)) ** (-this.messes));
+        animation.play();
+      } else {
+        this.ValorParcela = (this.emprestimo * (this.juros / 100)) / (1 - (1 + (this.juros / 100)) ** (-this.messes));
+        const alert = await this.alertController.create({
+          header: 'Cuidado',
+          message: `Esse empréstimo pode ser arriscado.`,
+          buttons: ['OK']
+        });
+        await alert.present();
+        animation.play();
+      }
+    }
+  }
+}
