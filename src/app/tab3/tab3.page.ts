7bdb8e278a956f51714707c93459ff77d2d65a6b
KogasaTatara <tanuke.p2@gmail.com>
2025-06-09 11:05:39 -0300

    FumoFumo

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
-import { RangeCustomEvent } from '@ionic/angular';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AlertController, RangeCustomEvent } from '@ionic/angular';
+import { Animation, createAnimation } from '@ionic/core';
 import { Despesas } from '../model/Expences';
 
 @Component({
@@ -10,34 +10,44 @@ import { Despesas } from '../model/Expences';
   standalone: false,
 })
 export class Tab3Page {
+  @ViewChild('palhacada', { static: false }) palhacada!: ElementRef<HTMLImageElement>;
+  constructor(private alertController: AlertController) { }
+  moradia = 0;
+  alimentacao = 0;
+  transporte = 0;
+  lazer = 0;
+  numero7 = 0;
+  ValorTotal = 0;
+  onIonChange(ev: RangeCustomEvent) {
+    this.numero7 = parseInt(ev.detail.value.toString())
+  }
 
-constructor(private alertController: AlertController) {}
-moradia = 0;
-alimentacao = 0;
-transporte = 0;
-lazer = 0;
-numero7 = 0;
-ValorTotal = 0;
-onIonChange(ev:RangeCustomEvent) {
-  this.numero7=parseInt(ev.detail.value.toString())
+  ListaDespesas: Despesas[] = [{
+    moradia: this.moradia,
+    alimentacao: this.alimentacao,
+    transporte: this.transporte,
+    lazer: this.lazer
+  }]
+
+  async Calcular() {
+
+    if (this.palhacada) {
+      const animation: Animation = createAnimation()
+        .addElement(this.palhacada.nativeElement)
+        .duration(1000)
+        .fromTo('opacity', '0', '1');
+      this.ValorTotal = this.moradia + this.alimentacao + this.transporte + this.lazer;
+      animation.play();
+      if (this.ValorTotal > this.numero7) {
+        const alert = await this.alertController.create({
+          header: 'Cuidado',
+          message: 'Você está gastando mais do que você ganha, reveja seus gastos',
+          buttons: ['OK']
+        });
+        await alert.present();
+
+      };
+    };
+  };
 }
 
-ListaDespesas: Despesas[] = [{
-  moradia: this.moradia,
-  alimentacao: this.alimentacao,
-  transporte: this.transporte,
-  lazer: this.lazer
-}]
-
-async Calcular() {
-  this.ValorTotal = this.moradia + this.alimentacao + this.transporte + this.lazer;
-  if (this.ValorTotal > this.numero7) {
-    const alert = await this.alertController.create({
-      header: 'Cuidado',
-      message: 'Você está gastando mais do que você ganha, reveja seus gastos',
-      buttons: ['OK']
-    });
-    await alert.present();
-  };
-};
-};
